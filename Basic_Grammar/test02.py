813a57475129cc2efaa7a22fbc1478a493ecc47c
tomori <tomori.gypsophila@foxmail.com>
2025-12-05 20:56:28 +0800

    learn start

---
+++ Basic_Grammar/test02.py
@@ -0,0 +1,8 @@
+age = 25
+city = "Shanghai"
+years_in_city =30
+
+if years_in_city > age:
+    print("??? are you sure?")
+else:
+    print("ok it is fine")
---
+++ Basic_Grammar/test1.py
@@ -0,0 +1,15 @@
+iphone_price = 8999
+iphone_brand = "Apple"
+print("my phone's brand is:", iphone_brand)
+print("my phone's price is:", iphone_price)
+
+iphone_price = 7999 # get a discount
+print("after a discount is:", iphone_price)
+
+my_city = "北京"
+years_in_city = 5
+future_years = 3
+total_years = years_in_city + future_years
+# print("I am in ", my_city, "I have lived for", years_in_city, "and the future year is", future_years, "will totally live", total_years)
+print(f"I live in {my_city}. I've been here for {years_in_city} years, and plan to stay for {future_years} more. Total will be {total_years} years.")
+# 格式化字符串，内嵌，看起来更优雅
