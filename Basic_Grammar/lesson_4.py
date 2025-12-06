7f343d79707939688944be132dd7af01ed440e3a
tomori <tomori.gypsophila@foxmail.com>
2025-12-06 15:44:28 +0800

    dictionary

---
+++ Basic_Grammar/lesson_4.py
@@ -0,0 +1,7 @@
+detect_object_ids = [5, 2, 8, 2, 1]
+print(detect_object_ids)
+
+detect_object_ids[0] = 10
+print(detect_object_ids)
+
+print(len(detect_object_ids))
---
+++ Basic_Grammar/lesson_5.py
@@ -0,0 +1,5 @@
+detect_object_ids = [10, 2, 8, 2, 1]
+for item in detect_object_ids:
+    print(item)
+    if item >= 5:
+        print("This is a large object")
